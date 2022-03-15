511fe6b928245ad87277bd4fa8b7ad1b1bf5cbb6
Bruna Maria <bruna.mariap@outlook.com>
2022-03-14 23:33:10 -0300

    modificação de configurações

--- code/tile.py
+++ code/tile.py
@@ -1,21 +1,21 @@
 import pygame
-from settings import *
+from read_json import settings
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))):
+    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((settings['general_settings']['tilesize'], settings['general_settings']['tilesize']))):
         super().__init__(groups)
         self.sprite_type = sprite_type
         self.image = surface
         if sprite_type == 'objects':
             # deslocar o sprite
-            self.rect = self.image.get_rect(topleft=(pos[0], pos[1]-TILESIZE))
+            self.rect = self.image.get_rect(topleft=(pos[0], pos[1]-settings['general_settings']['tilesize']))
         else:
             self.rect = self.image.get_rect(topleft=pos)
 
         if sprite_type == 'boundary':
             self.hitbox = self.rect.inflate(-40, -10)
         elif sprite_type == 'objects':
-            self.hitbox = self.rect.inflate(0, -TILESIZE)
+            self.hitbox = self.rect.inflate(0, -settings['general_settings']['tilesize'])
         else:
             self.hitbox = self.rect.inflate(0, -10)
